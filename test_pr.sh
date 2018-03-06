bdbda7d67f4d592f0b4fc14b5a89c6adddee189e
tahsinrahman <tahsynx@gmail.com>
2018-03-06 18:20:17 +0600

    test

--- test_pr.sh
+++ test_pr.sh
@@ -6,5 +6,6 @@ CURRENT_DIR=$(pwd)
 mkdir -p $GOPATH/src/github.com/appscode
 cp -r pull-request $GOPATH/src/github.com/appscode
 cd $GOPATH/src/github.com/appscode/pull-request
+git rev-parse HEAD
 go build -v
 go test -v ./... &> $CURRENT_DIR/test_result/message
