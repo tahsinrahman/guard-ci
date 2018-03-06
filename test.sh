ee361e2d95a47ecc0f1f5797140891d1ce45fb06
tahsinrahman <tahsynx@gmail.com>
2018-03-06 16:59:53 +0600

    fixed test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-pwd
-ls
 curr_dir=$pwd
 cd src/github.com/appscode/guard
 go install -v
-go test -v ./... > curr_dir/test_result/message
+go test -v ./... > curr_dir/$test_result/message
