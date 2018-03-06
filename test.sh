b61671282cbad7d73c114cc55d458fc58a672405
tahsinrahman <tahsynx@gmail.com>
2018-03-06 17:38:19 +0600

    test

--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@ set -x -e
 
 CURRENT_DIR=$(pwd)
 mkdir -p $GOPATH/src/github.com/appscode
-mv guard $GOPATH/src/github.com/appscode
+cp guard $GOPATH/src/github.com/appscode
 cd $GOPATH/src/github.com/appscode/guard
+ls
 go build -v
 go test -v ./... > $CURRENT_DIR/test_result/message
